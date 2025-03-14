35a1b863e1b89e9241046c50dd7846b134b99903
dheeraj-Joshi0208 <dheerajjoshi0208@gmail.com>
2025-03-15 01:20:31 +0530

    Create toggle background.

---
+++ ToogleBackgroundColor/src/App.jsx
@@ -0,0 +1,14 @@
+import ToggleBackgroundColor from "./component/ToogleBackgroundColor"
+
+function App() {
+
+  return (
+   <>
+   <div>
+    <ToggleBackgroundColor/>
+   </div>
+   </>
+  )
+}
+
+export default App
